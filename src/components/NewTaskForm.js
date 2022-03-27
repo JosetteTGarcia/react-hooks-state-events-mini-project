49ca5c73d47e5275d13512d90bc716daa8cb1927
Josette Garcia <josettetgarcia@gmail.com>
2022-03-26 22:27:22 -0500

    commit tree d643eda0e299d6c9ea9f3e8d2ee29e7babb802e5

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,16 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewTaskForm() {
+function NewTaskForm({categories, onTaskFormSubmit}) {
+  const [text, setText] = useState("");
+  const [category, setCategory] = useState("Food");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    onTaskFormSubmit({text, category})
+    setText("")
+    setCategory("Food")
+  }
+
+
+  const categoryOptions = categories.map((category) =>{
+    if (category !== "All"){
+    return(
+      <option key={category}>
+        {category}
+      </option>
+    )
+    }
+  })
   return (
-    <form className="new-task-form">
+    <form className="new-task-form" onSubmit={handleSubmit}>
       <label>
         Details
-        <input type="text" name="text" />
+        <input 
+          type="text" 
+          name="text" 
+          onChange= {(event) => setText(event.target.value)}
+        />
       </label>
       <label>
         Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
+        <select 
+          name="category"
+          onChange= {(event) => setCategory(event.target.value)}
+        >
+          {categoryOptions}
         </select>
       </label>
       <input type="submit" value="Add task" />
@@ -18,4 +45,4 @@ function NewTaskForm() {
   );
 }
 
-export default NewTaskForm;
+export default NewTaskForm; 
